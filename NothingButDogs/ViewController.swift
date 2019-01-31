e39bac35c3a750bc8bad7b04daa5e10c65e4cabf
(thatnaoki) <muroya.naoki@gmail.com>
2019-01-31 10:51:35 +0900

    Initial Commit

---
+++ NothingButDogs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NothingButDogs
+//
+//  Created by Naoki Muroya on 2019/01/31.
+//  Copyright © 2019 thatnaoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
